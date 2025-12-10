9d7aad24866ac8847e31617f43091bd037f3d9b3
Alejandro Téllez <119206974+Alextc35@users.noreply.github.com>
2025-12-10 11:29:41 +0100

    fix: solución en fallo de login salía el error dos veces

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -18,12 +18,6 @@
         </div>
 
         <button class="btn btn-primary w-100">Entrar</button>
-
-        @if($errors->any())
-            <div class="alert alert-danger mt-3">
-                {{ $errors->first() }}
-            </div>
-        @endif
     </form>
 </div>
 @endsection
